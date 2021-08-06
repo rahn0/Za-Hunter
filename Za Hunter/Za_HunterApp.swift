a22a9070818d33a7cb8db7e126169fc9e225a11c
rahn <ryanahn2000@gmail.com>
2021-08-05 19:51:03 -0700

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Ryan Ahn on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
